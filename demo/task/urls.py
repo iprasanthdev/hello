2f4cb7d18b71dbcbeb83665c018e250d9aeb46ca
iprasanthdev <j.prasanth.me98@gamil.com>
2021-08-05 23:42:10 +0530

    coonectivity of add and index page is established

--- demo/task/urls.py
+++ demo/task/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-app_name = "tasks"
+app_name = "tas"
 urlpatterns =[
     path("", views.index, name="index"),
     path("add/", views.add, name="add")
--- demo/task/views.py
+++ demo/task/views.py
@@ -1,11 +1,29 @@
+from django.urls import reverse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
 
-task = ['food','cloths','living room work']
+tasks = ["new"]
+
+class NewForm(forms.Form):
+    task = forms.CharField(label="Add your task")
 # Create your views here.
 def index(request):
     return render(request,"task/index.html",
-    {"tasks":task}
+    {"taskdic":tasks}
     )
 
 def add(request):
-    return render(request,"task/add.html")
+    if request.method == "POST":
+        form = NewForm(request.POST)
+        if form.is_valid():
+            task=form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tas:index"))
+        else:
+            return render(request,"task/add.html",{
+               "form":form
+             })
+    return render(request,"task/add.html",{
+        "form":NewForm()
+    })
